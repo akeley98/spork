dbdb3ea03dd8d267daf108e80c70708a956c9165
David Zhao Akeley <dza724@gmail.com>
2024-12-06 14:55:31 -0500

    Update gemm_loop.py

--- gemm_loop.py
+++ gemm_loop.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-import time, os
+import sys, time, os
 
 exe_path = "gemm/gemm"
 gemm_timestamp = None
@@ -10,7 +10,9 @@ while time.time() - watchdog_timestamp <= timeout:
     time.sleep(0.001)
     tmp_timestamp = os.stat(exe_path).st_mtime
     if tmp_timestamp != gemm_timestamp:
+        print(time.strftime("\x1b[34mSTART\x1b[0m %H:%M:%S", time.localtime()), file=sys.stderr)
         os.system(exe_path)
+        print("\x1b[34mEND\x1b[0m", file=sys.stderr)
         gemm_timestamp = tmp_timestamp
         watchdog_timestamp = time.time()
         
