8458df6ccb5c21abc2428645d08569180e780436
David Zhao Akeley <dza724@gmail.com>
2025-04-21 13:19:04 -0400

    fail fail fail

--- edited_xgemm_Sm90/edited.cuh
+++ edited_xgemm_Sm90/edited.cuh
@@ -256,44 +256,40 @@ EXO_CUDA_INLINE uint64_t exo_matrix_descriptor(const void* smem_ptr, uint32_t mn
 }
 
 /* Required by Sm90_mma_async_tf32(d,a,b,n=128) */
-template <unsigned swizzle_bits_a, unsigned swizzle_bits_b>
-EXO_CUDA_INLINE void exo_wgmma_mma_async_m64n128k8_f32_tf32(
-        exo_Sm90_RmemMatrixD<float, 64>& d, const void* smem_a, const void* smem_b,
-        unsigned m_matrix_stride, unsigned n_matrix_stride, unsigned k_matrix_stride)
-{
-    auto desc_a = exo_matrix_descriptor<swizzle_bits_a>(smem_a, m_matrix_stride, k_matrix_stride);
-    auto desc_b = exo_matrix_descriptor<swizzle_bits_b>(smem_b, n_matrix_stride, k_matrix_stride);
-    asm volatile(
-                "{ // exo_wgmma_mma_async_m64n128k8_f32_tf32 \n\t"
-                  ".reg .pred p;\n\t"
-                  "setp.ne.b32 p, %66, 0;\n\t"
-                  "wgmma.mma_async.sync.aligned.m64n128k8.f32.tf32.tf32\n\t"
-                  "{%0, %1, %2, %3, %4, %5, %6, %7, %8, %9, %10, %11, %12, %13, %14, %15, %16, %17, %18, %19, %20, %21, %22, %23, %24, %25, %26, %27, %28, %29, %30, %31, %32, %33, %34, %35, %36, %37, %38, %39, %40, %41, %42, %43, %44, %45, %46, %47, %48, %49, %50, %51, %52, %53, %54, %55, %56, %57, %58, %59, %60, %61, %62, %63},\n\t"
-                  " %64, "
-                  " %65, "
-                  " p,    1,  1;\n"
-                "}\n"
-                  : "+f"(d.d[0]), "+f"(d.d[1]), "+f"(d.d[2]), "+f"(d.d[3]), "+f"(d.d[4]), "+f"(d.d[5]), "+f"(d.d[6]), "+f"(d.d[7]), "+f"(d.d[8]), "+f"(d.d[9]), "+f"(d.d[10]), "+f"(d.d[11]), "+f"(d.d[12]), "+f"(d.d[13]), "+f"(d.d[14]), "+f"(d.d[15]), "+f"(d.d[16]), "+f"(d.d[17]), "+f"(d.d[18]), "+f"(d.d[19]), "+f"(d.d[20]), "+f"(d.d[21]), "+f"(d.d[22]), "+f"(d.d[23]), "+f"(d.d[24]), "+f"(d.d[25]), "+f"(d.d[26]), "+f"(d.d[27]), "+f"(d.d[28]), "+f"(d.d[29]), "+f"(d.d[30]), "+f"(d.d[31]), "+f"(d.d[32]), "+f"(d.d[33]), "+f"(d.d[34]), "+f"(d.d[35]), "+f"(d.d[36]), "+f"(d.d[37]), "+f"(d.d[38]), "+f"(d.d[39]), "+f"(d.d[40]), "+f"(d.d[41]), "+f"(d.d[42]), "+f"(d.d[43]), "+f"(d.d[44]), "+f"(d.d[45]), "+f"(d.d[46]), "+f"(d.d[47]), "+f"(d.d[48]), "+f"(d.d[49]), "+f"(d.d[50]), "+f"(d.d[51]), "+f"(d.d[52]), "+f"(d.d[53]), "+f"(d.d[54]), "+f"(d.d[55]), "+f"(d.d[56]), "+f"(d.d[57]), "+f"(d.d[58]), "+f"(d.d[59]), "+f"(d.d[60]), "+f"(d.d[61]), "+f"(d.d[62]), "+f"(d.d[63])
-                  :  "l"(desc_a), "l"(desc_b), "r"(d.scale_d));
-    d.scale_d = 1;
+#define exo_wgmma_mma_async_m64n128k8_f32_tf32(swizzle_bits_a, swizzle_bits_b, D, smem_a, smem_b, m_matrix_stride, n_matrix_stride, k_matrix_stride) \
+{ \
+    auto desc_a = exo_CudaUtil::exo_matrix_descriptor<swizzle_bits_a>(smem_a, m_matrix_stride, k_matrix_stride); \
+    auto desc_b = exo_CudaUtil::exo_matrix_descriptor<swizzle_bits_b>(smem_b, n_matrix_stride, k_matrix_stride); \
+    asm volatile( \
+                "{ // exo_wgmma_mma_async_m64n128k8_f32_tf32 \n\t" \
+                  ".reg .pred p;\n\t" \
+                  "setp.ne.b32 p, %66, 0;\n\t" \
+                  "wgmma.mma_async.sync.aligned.m64n128k8.f32.tf32.tf32\n\t" \
+                  "{%0, %1, %2, %3, %4, %5, %6, %7, %8, %9, %10, %11, %12, %13, %14, %15, %16, %17, %18, %19, %20, %21, %22, %23, %24, %25, %26, %27, %28, %29, %30, %31, %32, %33, %34, %35, %36, %37, %38, %39, %40, %41, %42, %43, %44, %45, %46, %47, %48, %49, %50, %51, %52, %53, %54, %55, %56, %57, %58, %59, %60, %61, %62, %63},\n\t" \
+                  " %64, " \
+                  " %65, " \
+                  " p,    1,  1;\n" \
+                "}\n" \
+                  : "+f"(D.d[0]), "+f"(D.d[1]), "+f"(D.d[2]), "+f"(D.d[3]), "+f"(D.d[4]), "+f"(D.d[5]), "+f"(D.d[6]), "+f"(D.d[7]), "+f"(D.d[8]), "+f"(D.d[9]), "+f"(D.d[10]), "+f"(D.d[11]), "+f"(D.d[12]), "+f"(D.d[13]), "+f"(D.d[14]), "+f"(D.d[15]), "+f"(D.d[16]), "+f"(D.d[17]), "+f"(D.d[18]), "+f"(D.d[19]), "+f"(D.d[20]), "+f"(D.d[21]), "+f"(D.d[22]), "+f"(D.d[23]), "+f"(D.d[24]), "+f"(D.d[25]), "+f"(D.d[26]), "+f"(D.d[27]), "+f"(D.d[28]), "+f"(D.d[29]), "+f"(D.d[30]), "+f"(D.d[31]), "+f"(D.d[32]), "+f"(D.d[33]), "+f"(D.d[34]), "+f"(D.d[35]), "+f"(D.d[36]), "+f"(D.d[37]), "+f"(D.d[38]), "+f"(D.d[39]), "+f"(D.d[40]), "+f"(D.d[41]), "+f"(D.d[42]), "+f"(D.d[43]), "+f"(D.d[44]), "+f"(D.d[45]), "+f"(D.d[46]), "+f"(D.d[47]), "+f"(D.d[48]), "+f"(D.d[49]), "+f"(D.d[50]), "+f"(D.d[51]), "+f"(D.d[52]), "+f"(D.d[53]), "+f"(D.d[54]), "+f"(D.d[55]), "+f"(D.d[56]), "+f"(D.d[57]), "+f"(D.d[58]), "+f"(D.d[59]), "+f"(D.d[60]), "+f"(D.d[61]), "+f"(D.d[62]), "+f"(D.d[63]) \
+                  :  "l"(desc_a), "l"(desc_b), "r"(D.scale_d)); \
+    D.scale_d = 1; \
 }
 
 /* Required by Sm90_mma_write_d_col_major_tf32(dst,src,n=128) */
-template <bool ColumnMajor, typename Window, typename Reg, unsigned RegCount>
-EXO_CUDA_INLINE void exo_Sm90_store_d(Window dst, const exo_Sm90_RmemMatrixD<Reg, RegCount>& src)
-{
-    const uint32_t tid = threadIdx.x % 128u;
-    const uint32_t r_base = (tid / 32u) * 16u + (tid % 32u) / 4u;
-    const uint32_t c_base = (tid % 4u) * 2u;
-    #pragma unroll
-    for (uint32_t reg_index = 0; reg_index < RegCount; ++reg_index) {
-        const uint32_t r = r_base + ((reg_index % 4u) / 2u) * 8u;
-        const uint32_t c = c_base + (reg_index / 4u) * 8 + (reg_index % 2u);
-        // Strict aliasing violation, but we have essentially no choice.
-        auto dst_ptr = reinterpret_cast<uint32_t*>(
-                &dst.data[c * dst.strides[!ColumnMajor] + r * dst.strides[ColumnMajor]]);
-        *dst_ptr = reinterpret_cast<const uint32_t&>(src.d[reg_index]);
-    }
+// template <bool ColumnMajor, typename Window, typename Reg, unsigned RegCount>
+// EXO_CUDA_INLINE void exo_Sm90_store_d(Window dst, const exo_Sm90_RmemMatrixD<Reg, RegCount>& src)
+#define exo_Sm90_store_d(ColumnMajor, RegCount, dst, src) \
+{ \
+    const uint32_t tid = threadIdx.x % 128u; \
+    const uint32_t r_base = (tid / 32u) * 16u + (tid % 32u) / 4u; \
+    const uint32_t c_base = (tid % 4u) * 2u; \
+    for (uint32_t reg_index = 0; reg_index < RegCount; ++reg_index) { \
+        const uint32_t r = r_base + ((reg_index % 4u) / 2u) * 8u; \
+        const uint32_t c = c_base + (reg_index / 4u) * 8 + (reg_index % 2u); \
+        auto dst_ptr = reinterpret_cast<uint32_t*>( \
+                &dst.data[c * dst.strides[!ColumnMajor] + r * dst.strides[ColumnMajor]]); \
+        *dst_ptr = reinterpret_cast<const uint32_t&>(src.d[reg_index]); \
+    } \
 }
 
 }  // end namespace
@@ -475,7 +471,8 @@ exo_Cuda0_edited_Sm90_wgmma::exo_deviceTask(char* exo_smem, exo_SyncState& exo_s
           asm("wgmma.fence.sync.aligned;");
           for (int k_mma = 0; k_mma < 4; k_mma++) {
             for (int m_mma = 0; m_mma < 2; m_mma++) {
-              exo_CudaUtil::exo_wgmma_mma_async_m64n128k8_f32_tf32<1, 1>(
+              // exo_CudaUtil::exo_wgmma_mma_async_m64n128k8_f32_tf32<1, 1>(
+              exo_wgmma_mma_async_m64n128k8_f32_tf32(1, 1,
                     D_rmem[m_mma],
                     &A_smem[((k_iter) * (8192) + (8 * m_mma + 16 * wg) * (256) + 8 * k_mma) / 256].byte_offset((8 * k_mma) * 4),
                     &B_smem[((k_iter) * (4096) + 8 * k_mma) / 256].byte_offset((8 * k_mma) * 4),
@@ -530,7 +527,8 @@ exo_Cuda0_edited_Sm90_wgmma::exo_deviceTask(char* exo_smem, exo_SyncState& exo_s
           asm("wgmma.fence.sync.aligned;");
           for (int k_mma = 0; k_mma < 4; k_mma++) {
             for (int m_mma = 0; m_mma < 2; m_mma++) {
-              exo_CudaUtil::exo_wgmma_mma_async_m64n128k8_f32_tf32<1, 1>(
+              // exo_CudaUtil::exo_wgmma_mma_async_m64n128k8_f32_tf32<1, 1>(
+              exo_wgmma_mma_async_m64n128k8_f32_tf32(1, 1,
                     D_rmem[m_mma],
                     &A_smem[((k_iter % 4) * (8192) + (8 * m_mma + 16 * wg) * (256) + 8 * k_mma) / 256].byte_offset((8 * k_mma) * 4),
                     &B_smem[((k_iter % 4) * (4096) + 8 * k_mma) / 256].byte_offset((8 * k_mma) * 4),
@@ -557,8 +555,10 @@ exo_Cuda0_edited_Sm90_wgmma::exo_deviceTask(char* exo_smem, exo_SyncState& exo_s
   exo_syncState.ReverseArrive0_ringbar(exo_smem, 1);
   if (int tmp = threadIdx.x; tmp < 256) {
     if ([[maybe_unused]] int wg = (threadIdx.x / 128); 1) {
+      #pragma unroll
       for (int m_mma = 0; m_mma < 2; m_mma++) {
-        exo_CudaUtil::exo_Sm90_store_d<true>(((struct exo_win_2f32){ &exo_deviceArgs.C[(128 * exo_task.n_task) * (exo_deviceArgs.M) + 64 * m_mma + 128 * wg + 256 * exo_task.m_task], { exo_deviceArgs.M, 1 } }), D_rmem[m_mma]);
+        // exo_CudaUtil::exo_Sm90_store_d<true>(((struct exo_win_2f32){ &exo_deviceArgs.C[(128 * exo_task.n_task) * (exo_deviceArgs.M) + 64 * m_mma + 128 * wg + 256 * exo_task.m_task], { exo_deviceArgs.M, 1 } }), D_rmem[m_mma]);
+        exo_Sm90_store_d(true, 64, ((struct exo_win_2f32){ &exo_deviceArgs.C[(128 * exo_task.n_task) * (exo_deviceArgs.M) + 64 * m_mma + 128 * wg + 256 * exo_task.m_task], { exo_deviceArgs.M, 1 } }), D_rmem[m_mma]);
       }
     }
   }
