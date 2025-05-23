8c75193f3a4af32ef63cd3650b7b55b3f8a085b7
David Zhao Akeley <dza724@gmail.com>
2025-05-22 20:41:28 -0400

    More work on new window proposal

--- docs/su_samples.py
+++ docs/su_samples.py
@@ -56,7 +56,7 @@ Sm80_mma_load_a_tf32(A_rmem[k_seq,:,:],  # Runtime: RMEM dst
 # TeX: version avx_example 1
 # TeX: begin avx_example
 # TeX: color line *
-#        yyyy  b                                  b
+#        y  y  b                                  b
 foo: f32[4, 6, 8] @ AVX2  # Expected vector size: 8
 # Lowered C++
 # TeX: color line *
@@ -67,35 +67,39 @@ foo: f32[4, 6, 8] @ AVX2  # Expected vector size: 8
 # TeX: version warp_example 1
 # TeX: begin warp_example
 # TeX: color line *
-#        rrrr  y  bbbbb                                              bbbbbb
-foo: f32[2, 4, 6, 8, 16] @ Sm80_RmemMatrixA  # Expected matrix tile: 8 x 16
+#        r  r  y  bb  b                                              bbbbbb
+foo: f32[2, 4, 6, 16, 8] @ Sm80_RmemMatrixD  # Expected matrix tile: 16 x 8
 for mw in cuda_threads(0, 2, unit=4 * cuda_warp):
     for nw in cuda_threads(0, 4, unit=cuda_warp):
         for array_idx in seq(0, 6):
             # Ownership of foo distributed to 2 x 4 grid of warps
             # TeX: color line *
-            #   rr  rr
-            foo[mw, nw, array_idx, :, :]
+            #                        rr  rr
+            Sm80_mma_zero_d_tf32(foo[mw, nw, array_idx, :, :])
 # Lowered C++
 # TeX: color line *
 #              y  b                             rrrrr
   unsigned foo[6][4];  // Executed uniformly by 2 x 4 grid of warps
 # TeX: color line *
-                        # bbbbbb               b
-                       // 8 x 16 matrix tile = 4 registers per thread (32 threads/warp total)
+            # bbbbbb               b
+           // 16 x 8 matrix tile = 4 registers per thread (32 threads/warp total)
+  for (int array_idx = 0; array_idx < 6; array_idx++) {
+    exo_CudaUtil::Sm80_mma_zero_d(foo[array_idx]);
+  }
 # TeX: end warp_example
 
 # TeX: version avx_window 1
 # TeX: begin avx_window
 # TeX: color line *
-#          bbbbbbbb
+#                 b
 src: [f32][packed:8] @ AVX2
 # TeX: end avx_window
 
 # TeX: version tma_window 2
 # TeX: begin tma_window[0]
+# Innermost dimension cannot be strided
 # TeX: color line *
-#          yyyyyyyyyyyyyyyyyyyyyyyy
+#                  yyyy        yyyy
 src: [f32][strided:box0, dense:box1] @ Sm90_CUtensorMap
 # TeX: end tma_window[0]
 # TeX: begin tma_window[1]
@@ -106,7 +110,7 @@ src: [f32][strided:box0, dense:box1] @ Sm90_CUtensorMap
 #                   rrrrrrr
 # TMA multicasts to 2 CTAs' shared memory
 # TeX: color line *
-#          rrrrrrrrrrrrr  yyyyyyyyyyyyy  bbbbbbbbbbbbbb
+#                      r         yyyyyy         b  bbbb
 dst: [f32][distributed:2, dense: box0/8, packed:8, box1] @ Sm90_CUtensorMap
 # TeX: end tma_window[1]
 
@@ -138,3 +142,68 @@ struct <sname>
     int32_t offsets[...];
 };
 # TeX: end pointer_offsets
+
+
+# TeX: version 2f2c 1
+# TeX: begin 2f2c
+struct exo_win_2f32c{
+    const float * const data;
+    const int_fast32_t strides[2];
+};
+# TeX: end 2f2c
+
+# TeX: version avx_before 1
+# TeX: begin avx_before[0]
+    def window(cls, basetyp, baseptr, indices, strides, srcinfo)
+        # ...
+        idxs = indices[:-1] or ""
+        if idxs:
+            idxs = "[" + "][".join(idxs) + "]"
+    # ...
+    foo[1,3,:]    # Lowers to foo[1][3]
+    foo[1:9,3,6]  # Also lowers to foo[1][3]
+# TeX: end avx_before[0]
+
+# TeX: version is_win 1
+# TeX: begin is_win[0]
+    @classmethod
+    # TeX: color line *
+    #               yyyyyyyyyyyyyyyyyyyyyy
+    def window(cls, basetyp: LoopIR.Tensor, baseptr, indices, strides, srcinfo):
+        offset = generate_offset(indices, strides)
+
+        # TeX: color line *
+        #  yyyyyyyyyyyyyyyy
+        if basetyp.is_win():
+            baseptr = f"{baseptr}.data"
+
+        return f"{baseptr}[{offset}]"
+# TeX: end is_win[0]
+
+# TeX: version tma_proc 1
+# TeX: begin tma_proc[0]
+@proc
+# TeX: color line *
+#            ggggggggggg
+def tma_proc(tensorMap_a: [f32][128, 32] @ Sm90_tensorMap(128, 128, 32),
+# TeX: color line *
+#            vvvvvvvvvvv
+             tensorMap_b: [f32][256, 32] @ Sm90_tensorMap(128, 256, 32)):
+
+void tma_proc( void *ctxt,
+# TeX: color line *
+#             gggggggggggggggggggg                                                ggggggggggg
+  CUtensorMap exo_data_tensorMap_a, struct exo_win_2f32_Sm90_tensorMap_128_128_32 tensorMap_a,
+# TeX: color line *
+#             vvvvvvvvvvvvvvvvvvvv                                                vvvvvvvvvvv
+  CUtensorMap exo_data_tensorMap_b, struct exo_win_2f32_Sm90_tensorMap_128_256_32 tensorMap_b
+) {
+# TeX: end tma_proc[0]
+
+# TeX: version encode_window 1
+# TeX: begin encode_window
+(struct {sname}) {{
+    {dataptr} + ({strides[0]}) * ({array_offsets[0]}) + ({strides[1]}) * ({array_offsets[1]}) + ...,
+    {{ ({strides[0]}), ({strides[1]}), ... }}
+}}
+# TeX: end encode_window
