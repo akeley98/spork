19878f4934a5e4e035164debb99aa976eb99552e
David Zhao Akeley <dza724@gmail.com>
2025-07-25 17:18:26 -0400

    Use exocc new package support to avoid hacking sys.path for xgemm_Sm90

--- xgemm_Sm90_n128.py
+++ xgemm_Sm90_n128.py
@@ -1,6 +1,3 @@
-import sys, os
-sys.path.append(os.path.split(__name__)[0])
-
-from xgemm_Sm90 import make_Sm90_gemm
+from .xgemm_Sm90 import make_Sm90_gemm
 
 xgemm_Sm90_n128 = make_Sm90_gemm(128, 2, 2)
--- xgemm_Sm90_n256.py
+++ xgemm_Sm90_n256.py
@@ -1,6 +1,3 @@
-import sys, os
-sys.path.append(os.path.split(__name__)[0])
-
-from xgemm_Sm90 import make_Sm90_gemm
+from .xgemm_Sm90 import make_Sm90_gemm
 
 xgemm_Sm90_n256 = make_Sm90_gemm(256, 2, 1)
