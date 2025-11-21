6761e4e2ddb90dd530188640e560d3f1e74b50fc
David Zhao Akeley <dza724@gmail.com>
2025-11-21 17:08:33 -0500

    WIP new spork_b overview

--- docs/b_samples.py
+++ docs/b_samples.py
@@ -5,6 +5,50 @@ from exo.platforms.cuda import *
 M = 13
 N = 37
 
+# TeX: version loop_modes 1
+if False:
+    # TeX: begin loop_modes[0]
+    for _ in seq(_, _, pragma_unroll=_): _
+    for _ in par(_, _): _
+    for _ in cuda_tasks(_, _): _
+    for _ in cuda_threads(_, _, unit=_): _
+    # TeX: end loop_modes[0]
+
+# TeX: version OverviewThreads 1
+# TeX: begin OverviewThreads[0]
+@proc
+def overview_threads_example(num_tasks: size):
+    # CPU scope here.
+    for i in seq(0, 100):
+    # TeX: color line *
+      # ....
+        pass  # Still a typical CPU loop, 100 iterations
+    with CudaDeviceFunction(clusterDim=1, blockDim=256):
+        # CUDA scope inside this CudaDeviceFunction block.
+        for task_id in cuda_tasks(0, num_tasks):
+            # CTA-scope here, also cluster-scope because clusterDim=1.
+            for w in cuda_threads(0, 8, unit=cuda_warp):
+                # Warp-scope here.
+                # blockDim=256 threads subdivided into 8 separate warps indexed by w.
+                for t in cuda_threads(0, 32, unit=cuda_thread):
+                    # Thread-scope here.
+                    for s in seq(0, 100):
+                      # TeX: color line *
+                      # ....
+                        pass  # Each thread does something 100 times.
+@proc
+def overview_distributed_memory_example(num_tasks: size):
+    with CudaDeviceFunction(clusterDim=1, blockDim=256):
+        for task_id in cuda_tasks(0, num_tasks):
+            tile: f32[8, 32, 8, 8] @ CudaRmem  # Registers must be sharded per-thread.
+            for w in cuda_threads(0, 8, unit=cuda_warp):  # Thread pitch = 32
+                for t in cuda_threads(0, 32, unit=cuda_thread):  # Thread pitch = 1
+                    for y in seq(0, 8):
+                        for x in seq(0, 8):
+                            # Deduction: tile[w, t, _, _] is stored on thread $32w + 1t$ of CTA.
+                            tile[w, t, y, x] = 0
+# TeX: end OverviewThreads[0]
+
 # TeX: version CudaDeviceFunction 1
 @proc
 def cuda_device_function_example():
@@ -39,5 +83,31 @@ if False:
     # TeX: color line *
     #                                               ....
     wrap_with_context(proc, block_cursor, CudaWarps(0, 1))
-    set_loop_mode(proc, loop_cursor, CudaTasks)
+    set_loop_mode(proc, loop_cursor, CudaTasks)  # sugar for CudaTasks()
     # TeX: end CudaDeviceFunction_scheduling[0]
+
+
+# TeX: version OverviewSyncExample 1
+# TeX: begin OverviewSyncExample[0]
+@proc
+def overview_sync_example(num_tasks: size):
+    with CudaDeviceFunction(clusterDim=2, blockDim=384):
+        for task_id in cuda_tasks(0, num_tasks):
+            # Cluster scope.
+            mbar: barrier[2] @ CudaMbarrier  # mbarrier variable.
+            for cta in cuda_threads(0, 2, unit=cuda_cta_in_cluster):
+                # CTA scope.
+                # cuda_in_order is a SyncTL,
+                # indicates only non-async instrs' effects are synchronized.
+                Fence(cuda_in_order, cuda_in_order)  # __syncthreads-equivalent
+                for w in cuda_threads(0, 12, unit=cuda_warp):
+                    # Warp scope.
+                    Fence(cuda_in_order, cuda_in_order)  # __syncwarp-equivalent
+                # Distributed memory: each element of mbar[...] allocated into its own CTA.
+                # Example of Arrive/Await using mbarrier mechanism
+                # (because mbar was annotated with @CudaMbarrier).
+                Arrive(cuda_in_order) >> mbar[cta]
+                # ...
+                Await(mbar[cta], cuda_in_order, ~0)
+                # Synchronization chapter explains ~0 (n).
+                # TeX: end OverviewSyncExample[0]
