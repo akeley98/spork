9b876d0a7dc557ae9c4423df460d03bf5c13932c
David Zhao Akeley <dza724@gmail.com>
2026-05-18 21:41:21 -0400

    ultraspork/Tensors.tex

--- docs/ultraspork_samples.py
+++ docs/ultraspork_samples.py
@@ -30,3 +30,61 @@ def overview_threads_example(Y_TASKS: size, X_TASKS: size):
                             pass  # Each thread does something 100 times.
 # TeX: end OverviewThreads[0]
 
+# Not a proc since this doesn't compile at all
+def mma_example():
+    # TeX: version mma_a 6
+    # TeX: begin mma_a[0]
+    word[2 @ (16 * bit)]
+    # TeX: end mma_a[0]
+
+    # TeX: begin mma_a[1]
+    # TeX: color line *
+    #   gggggggggg
+    row[4 @ thread, 2 @ (16 * bit)]
+    # TeX: end mma_a[1]
+
+    # TeX: begin mma_a[2]
+    matrix[
+        # TeX: color line *
+      # gggggggggggggggg
+        8 @ (4 * thread),               # M dimension
+        4 @ thread, 2 @ (16 * bit),     # K dimension
+    ]
+    # TeX: end mma_a[2]
+
+    # TeX: begin mma_a[3]
+    # TeX: begin mma_a[4]
+    matrices[
+        # TeX: color line mma_a[3]
+      # ggggggggggggggg
+        2 @ register_id, 8 @ (4 * thread),                  # M dimension
+        # TeX: color line mma_a[3]
+      # ggggggggggggggggggggg
+        2 @ (2 * register_id), 4 @ thread, 2 @ (16 * bit),  # K dimension
+    ]
+    # TeX: end mma_a[3]
+    # Free variables highlighted in yellow
+    matrices[m_reg, m_thr, k_reg, k_thr, k_packed] = (
+        # TeX: color line *
+        #yyyyyyyyyyyyy                      yyyyyyyyyyy
+        (base_register + m_reg + k_reg*2) * register_id +
+        # TeX: color line *
+        #yyyyyyyyyyy                      yyyyyyyyy
+        (base_thread + m_thr*4 + k_thr) * thread_id +
+        # TeX: color line *
+        #               yyy
+        k_packed * 16 * bit)
+    # TeX: end mma_a[4]
+
+    # TeX: begin mma_a[5]
+    # NB here the fast dimension is the right-most, which is opposite of CuTe
+    matrices[
+        [2 @ register_id, 8 @ (4 * thread)],                 # M dimension
+        [2 @ (2 * register_id), 4 @ thread, 2 @ (16 * bit)]  # K dimension
+    ]
+    # Not 1000% sure I did the math right:
+    matrices[m, k] = (
+        (base_register + DivAndMod(m, 8, 2) + DivAndMod(k, 8, 2) * 2) * register_id +
+        (base_thread + DivAndMod(m, 1, 8) * 4 + DivAndMod(k, 2, 4)) * thread_id +
+        DivAndMod(k, 1, 2) * 16 * bit)
+    # TeX: end mma_a[5]
